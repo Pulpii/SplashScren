fd77738171464bb64075f04e1aa9842476424cb0
pulpiicifpufmoll <dacedopulpillo@cifpfbmoll.eu>
2023-11-24 21:03:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/QuizSplashActivity.java
@@ -0,0 +1,52 @@
+package com.example.splashscreen;
+
+import android.content.Intent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TextView topLogo = (TextView) findViewById(R.id.TextViewTopTitle);
+        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        topLogo.startAnimation(fade1);
+
+        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        LayoutAnimationController controller = new LayoutAnimationController(spinin);
+
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
+
+        Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        TextView topLogo = (TextView) findViewById(R.id.TextViewTopTitle);
+        topLogo.clearAnimation();
+
+        TextView bottomLogo = (TextView) findViewById(R.id.TextViewBottomTitle);
+        bottomLogo.clearAnimation();
+
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.clearAnimation();
+        }
+    }
+}
